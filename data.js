871f41ffbe0191dfc0ac7e4bb514bcbce9c5b2c6
slow-key <100492032+slow-key@users.noreply.github.com>
2025-12-23 14:09:16 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,47 @@
+const initialPropertyData = [
+    {
+        "id": 1766516599991,
+        "name": "301",
+        "address": "",
+        "elec": "",
+        "water": "",
+        "gas": "",
+        "morg": "https://www.freedommortgage.com/get-started-online?utm_vertical=brandobs&utm_confid=svp5jktbw&utm_source=google&utm_medium=cpc&utm_campaign=seer_google_brand_search_b_acq_general_core_obs_brand&utm_campaignid=14307326574&utm_content=&utm_matchtype=e&utm_adgroupid=128779336249&utm_targetid=kwd-148051289&utm_loc_interest_ms=&utm_loc_physical_ms=9013141&utm_network=g&utm_device=c&utm_devicemodel=&utm_placement=&utm_target=&utm_adposition=&utm_keyword=freedom%20home%20mortgage&vt-n=g&vt-d=c&vt-dm=&vt-p=&vt-ap=&vt-k=freedom%20home%20mortgage&vt-c=738176028132&vt-mt=e&utm=brandobs_freedom%20home%20mortgage_14307326574_128779336249_738176028132&_st_custom_value=14307326574&customer_ID=995-716-7129&gad_source=1&gad_campaignid=14307326574&gbraid=0AAAAAD1bWIFuD7REsZO1RJaCXM_3n8SLC&gclid=CjwKCAiAmKnKBhBrEiwAaqAnZxv9O4rHb-El96Olfh2tg0qz_cKJVw4QtbNsXd3t2SRUEPuS7rFxbhoCYpQQAvD_BwE#loanPurpose"
+    },
+    {
+        "id": 1766516579696,
+        "name": "505",
+        "address": "",
+        "elec": "https://www.nespower.com/",
+        "water": "https://www.nashville.gov/departments/water/customers/payment-methods",
+        "gas": "https://www.piedmontng.com/home",
+        "morg": "https://yourmortgageonline.com/login"
+    },
+    {
+        "id": 1766516759909,
+        "name": "2324",
+        "address": "",
+        "elec": "https://www.nespower.com/",
+        "water": "https://www.nashville.gov/departments/water/customers/payment-methods",
+        "gas": "https://www.piedmontng.com/home",
+        "morg": "https://mypennymac.pennymac.com/SuccessfulLogout"
+    },
+    {
+        "id": 1766516708238,
+        "name": "3108",
+        "address": "",
+        "elec": "",
+        "water": "",
+        "gas": "",
+        "morg": "https://newrez.com/?utm_source=google&utm_medium=cpc&utm_campaign=Newrez_Branded_Search&utm_source=google-ads&utm_campaign=newrez_branded_search&utm_agid=174920881182&utm_term=newrez&creative=744411552612&device=c&placement=&gad_source=1&gad_campaignid=22409394222&gbraid=0AAAAADzHlgcGujcN5YVZ4y7ocXxWJNN-a&gclid=CjwKCAiAmKnKBhBrEiwAaqAnZzdf-FVogbkvK8YHe8wLCpyQYGIdg4H-8Qs2_bwHiuS8F3U_fJALpBoCQTEQAvD_BwE"
+    },
+    {
+        "id": 1766516638551,
+        "name": "4754",
+        "address": "",
+        "elec": "",
+        "water": "",
+        "gas": "",
+        "morg": "https://www.usbank.com/home-loans/mortgage.html"
+    }
+];
